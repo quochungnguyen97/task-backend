84d7a0eeb4ca27ed0be3e6ca578ae45c1ddf0c46
idobelieveinmiracle <quochung1997s@gmail.com>
2022-07-11 21:21:34 +0700

    First commit [Close #1]

---
+++ src/main/java/com/rose/tasksbackend/TasksServerApplication.java
@@ -0,0 +1,13 @@
+package com.rose.tasksbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rose/tasksbackend/TasksServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rose.tasksbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
