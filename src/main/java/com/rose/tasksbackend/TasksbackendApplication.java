9291eb438f5e131ac4daf11ef52df1663b9fa7c4
idobelieveinmiracle <quochung1997s@gmail.com>
2022-07-11 21:21:34 +0700

    First commit [Close #1]

---
+++ src/main/java/com/rose/tasksbackend/TasksbackendApplication.java
@@ -0,0 +1,13 @@
+package com.rose.tasksbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksbackendApplication.class, args);
+	}
+
+}
